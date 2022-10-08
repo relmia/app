22af44c4578b7eb4241afbf4f3defe995341c92f
Guillermo Tala <guille86598@gmail.com>
2022-10-08 01:50:52 -0500

    simple redirect flow

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,57 +1,65 @@
 // This is a script for deploying your contracts. You can adapt it to deploy
 // yours, or create new ones.
 
-const path = require("path");
+const path = require('path');
 
 async function main() {
   // This is just a convenience check
-  if (network.name === "hardhat") {
+  if (network.name === 'hardhat') {
     console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+      'You are trying to deploy a contract to the Hardhat Network, which' +
+        'gets automatically created and destroyed every time. Use the Hardhat' +
+        " option '--network localhost'",
     );
   }
 
   // ethers is available in the global scope
   const [deployer] = await ethers.getSigners();
-  const address = await deployer.getAddress()
-  console.log(
-    "Deploying the contracts with the account:",
-    address
-  );
+  const address = await deployer.getAddress();
+  console.log('Deploying the contracts with the account:', address);
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log('Account balance:', (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
+  const Token = await ethers.getContractFactory('TradeableCashflow');
+
+  /*  address owner,
+            string memory _name,
+            string memory _symbol,
+            ISuperfluid host,
+            ISuperToken acceptedToken
+        */
+
+  const token = await Token.deploy(ar);
   await token.deployed();
 
-  console.log("Contract address:", token.address);
+  console.log('Contract address:', token.address);
 
   // We also save the contract's artifacts and address in the frontend directory
   saveFrontendFiles(token);
 }
 
 function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts", network.name);
+  const fs = require('fs');
+  const contractsDir = path.join(__dirname, '..', 'frontend', 'src', 'contracts', network.name);
 
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
   }
 
   fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
-    JSON.stringify({ Token: token.address }, undefined, 2)
+    path.join(contractsDir, 'contract-address.json'),
+    JSON.stringify(
+      {
+        Token: token.address,
+      },
+      undefined,
+      2,
+    ),
   );
 
-  const TokenArtifact = artifacts.readArtifactSync("Token");
+  const TokenArtifact = artifacts.readArtifactSync('Token');
 
-  fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
-    JSON.stringify(TokenArtifact, null, 2)
-  );
+  fs.writeFileSync(path.join(contractsDir, 'Token.json'), JSON.stringify(TokenArtifact, null, 2));
 }
 
 main()
